2b25fe60015c2b8e3c00dcc0bc92a2116040b702
jiayuhan94 <hanjia@oregonstate.edu>
2017-02-01 09:32:39 -0800

    fireAt test file

--- src/test/edu/oregonstate/cs361/battleship/fireAtTest.java
+++ src/test/edu/oregonstate/cs361/battleship/fireAtTest.java
@@ -1,10 +1,16 @@
 package edu.oregonstate.cs361.battleship;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Created by jiayu on 2017-01-31.
+ * Created by jiayu on 2017-02-01.
  */
 class fireAtTest {
+    @Test
+    void main() {
+
+    }
 
 }
