ec92c423d51e1eb1592cd4a2613848bd49e45c38
jiayuhan94 <hanjia@oregonstate.edu>
2017-02-02 16:05:49 -0800

    random update
    random

--- src/main/java/edu/oregonstate/cs361/battleship/fireAt.java
+++ src/main/java/edu/oregonstate/cs361/battleship/fireAt.java
@@ -1,7 +1,21 @@
 package edu.oregonstate.cs361.battleship;
-
 /**
  * Created by Jiayu on 2017-01-30.
  */
+
 public class fireAt {
+    /** check if the fire is valid **/
+
+
+    //get the fire point and the game state
+
+    //check if there is any ship
+
+    //update the game state
+
+
+
+
+    public static void main(String[] args){
+    }
 }
---
+++ src/test/edu/oregonstate/cs361/battleship/fireAtTest.java
@@ -0,0 +1,9 @@
+package edu.oregonstate.cs361.battleship;
+
+/**
+ * Created by Jiayu on 2017-02-01.
+ */
+class fireAtTest {
+
+
+}
