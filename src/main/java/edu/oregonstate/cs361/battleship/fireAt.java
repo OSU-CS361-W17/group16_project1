251d665ec8983b086ea96282632abb9720ecfd54
jiayuhan94 <hanjia@oregonstate.edu>
2017-02-03 06:27:06 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/edu/oregonstate/cs361/battleship/Main.java
    #	src/main/java/edu/oregonstate/cs361/battleship/fireAt.java

--- src/main/java/edu/oregonstate/cs361/battleship/fireAt.java
+++ src/main/java/edu/oregonstate/cs361/battleship/fireAt.java
@@ -1,5 +1,8 @@
 package edu.oregonstate.cs361.battleship;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
+
 /**
  * Created by Jiayu on 2017-01-30.
  */
@@ -8,15 +11,47 @@ public class fireAt {
 
     //get the fire point and the game state
 
+
+    public void checkships(int x, int y){
+        int i;
+        i = 1;
+        NewModel nModel = new NewModel();
+        Point hitpoint =new Point(x,y);
+        Point Aircraftstart =new Point(nModel.getPlayerAircraftStart());
+
+        // grab the ship model
+
+
+            if(checkhits(hitpoint, startpoint, endpoint)){
+                nModel.setHit();
+            }
+
+    }
+
+
+
     //check if there is any ship
+    public boolean checkhits(Point thisshot, Point starts, Point ends){
+
+        if (thisshot.x == starts.x ){
+            if (thisshot.y <= ends.y && thisshot.y >= starts.y){
+                return true;
+            }
+        }else if(thisshot.y == starts.y){
+            if (thisshot.x <= ends.x && thisshot.x >= starts.x) {
+                return true;
+            }
+        }
+            return false;
+    }
+
+
 
     //update the game state
 
 
 
 
-    public static void main(String[] args) {
-    }
 
 
 
