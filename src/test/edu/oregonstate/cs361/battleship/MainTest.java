a3e702921d472c7acb882012ca3fa76fb9d428d8
nodadragon <edwardne@oregonstate.edu>
2017-02-03 21:12:07 -0800

    Added tests for placeship in maintest

--- src/test/edu/oregonstate/cs361/battleship/MainTest.java
+++ src/test/edu/oregonstate/cs361/battleship/MainTest.java
@@ -49,10 +49,103 @@ class MainTest {
     }
 
     @Test
-    public void testPlaceShip() {
+    public void testPlaceShip1() {
         TestResponse res = request("POST", "/placeShip/aircraftCarrier/1/1/horizontal");
         assertEquals(200, res.status);
-        assertEquals("SHIP", res.body);
+        assertEquals("SHIP",res.body);
+    }
+    //Test invalid horizontal placement
+    @Test
+    public void testPlaceShip2() {
+        TestResponse res = request("POST", "/placeShip/destroyer/11/10/horizontal");
+        assertEquals(200, res.status);
+        assertEquals("Invalid",res.body);
+    }
+    //Test invalid vertical placement
+    @Test
+    public void testPlaceShip3() {
+        TestResponse res = request("POST", "/placeShip/destroyer/9/11/vertical");
+        assertEquals(200, res.status);
+        assertEquals("Invalid",res.body);
+    }
+    @Test
+    public void testPlaceShip4() {
+        TestResponse res = request("POST", "/placeShip/submarine/5/4/vertical");
+        assertEquals(200, res.status);
+        assertEquals("SHIP",res.body);
+    }
+    @Test
+    public void testPlaceShip5() {
+        TestResponse res = request("POST", "/placeShip/cruiser/7/7/vertical");
+        assertEquals(200, res.status);
+        assertEquals("SHIP",res.body);
+    }
+    @Test
+    public void testPlaceShip6() {
+        TestResponse res = request("POST", "/placeShip/battleship/5/4/vertical");
+        assertEquals(200, res.status);
+        assertEquals("SHIP",res.body);
+    }
+    @Test
+    public void testgetShipLength1() {
+        String ship = "battleship";
+        int result = Main.getShipLength(ship);
+        assertEquals(4, result);
+    }
+    @Test
+    public void testgetShipLength2() {
+        String ship = "cruiser";
+        int result = Main.getShipLength(ship);
+        assertEquals(3, result);
+    }
+    @Test
+    public void testgetShipLength3() {
+        String ship = "aircraftCarrier";
+        int result = Main.getShipLength(ship);
+        assertEquals(5, result);
+    }
+    @Test
+    public void testgetShipLength4() {
+        String ship = "submarine";
+        int result = Main.getShipLength(ship);
+        assertEquals(3, result);
+    }
+    @Test
+    public void testgetShipLength5() {
+        String ship = "destroyer";
+        int result = Main.getShipLength(ship);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testgetIdNum1() {
+        String ship = "submarine";
+        int result = Main.getIdNum(ship);
+        assertEquals(5, result);
+    }
+    @Test
+    public void testgetIdNum2() {
+        String ship = "destroyer";
+        int result = Main.getIdNum(ship);
+        assertEquals(4, result);
+    }
+    @Test
+    public void testgetIdNum3() {
+        String ship = "cruiser";
+        int result = Main.getIdNum(ship);
+        assertEquals(3, result);
+    }
+    @Test
+    public void testgetIdNum4() {
+        String ship = "battleship";
+        int result = Main.getIdNum(ship);
+        assertEquals(2, result);
+    }
+    @Test
+    public void testgetIdNum5() {
+        String ship = "aircraftCarrier";
+        int result = Main.getIdNum(ship);
+        assertEquals(1, result);
     }
 
     private TestResponse request(String method, String path) {
